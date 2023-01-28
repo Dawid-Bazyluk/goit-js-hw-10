bbafdd68600aca2276bb4987a5ae913bf38a01df
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-01-28 20:56:39 +0100

    created function fetchCountries

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,14 @@
 export function fetchCountries(name) {
   return fetch(
     'https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages'
-    );
-    
+  )
+    .then(r => r.json())
+    .catch(e => console.log('Error:', e));
 }
+
+fetch(
+  'https://restcountries.com/v3.1/name/poland?fields=name,capital,population,flags,languages'
+)
+  .then(r => r.json())
+  .then(r => console.log(r))
+  .catch(e => console.log('Error:', e));
