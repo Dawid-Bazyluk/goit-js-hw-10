e94cc715a1a832230e373169e33407a122047193
Dawid-Bazyluk <dawid.bazyluk@gmail.com>
2023-01-30 23:41:51 +0000

    Deploying to gh-pages from @ Dawid-Bazyluk/goit-js-hw-10@5088b1a1ade61d16632d7b980bff1905935132f3 🚀

--- index.43216ef9.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/poland?fields=name,capital,population,flags,languages").then((function(n){return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return console.log("Error:",n)}));
-//# sourceMappingURL=index.43216ef9.js.map
--- index.cb21350c.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/poland?fields=name,capital,population,flags,languages").then((o=>o.json())).then((o=>console.log(o))).catch((o=>console.log("Error:",o)));
-//# sourceMappingURL=index.cb21350c.js.map
